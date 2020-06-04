570aab18d6e1eb96d3d14e88349ccc1063d42f92
khaittran <erick84khai@gmail.com>
2020-06-04 16:51:50 -0700

    Updates

---
+++ precache-manifest.158ec7c737aefe8f2703ea5e003d6130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798001137077d29b7a48b219213d4ce3",
+    "url": "/workroom/index.html"
+  },
+  {
+    "revision": "8b2891396a92ee732c4f",
+    "url": "/workroom/static/css/2.6cc87aa2.chunk.css"
+  },
+  {
+    "revision": "92438ef99c361e6d5c1c",
+    "url": "/workroom/static/css/main.27aac6a4.chunk.css"
+  },
+  {
+    "revision": "8b2891396a92ee732c4f",
+    "url": "/workroom/static/js/2.f8d0466d.chunk.js"
+  },
+  {
+    "revision": "5c2b152c472952b70af4e9ec97fe8055",
+    "url": "/workroom/static/js/2.f8d0466d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92438ef99c361e6d5c1c",
+    "url": "/workroom/static/js/main.0e66fe0b.chunk.js"
+  },
+  {
+    "revision": "5f8f3fbc95dc5de8d2be",
+    "url": "/workroom/static/js/runtime-main.2970c8ae.js"
+  }
+]);
